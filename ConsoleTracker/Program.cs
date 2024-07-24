49739724164d6f66e8d15513a729bfc5485524b5
Karli Nadsady <122899636+kn274621@users.noreply.github.com>
2024-07-24 21:17:33 +0000

    program can write to file

---
+++ ConsoleTracker/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Create a string array with the lines of text
+        string[] lines = { "First line", "Second line", "Third line" };
+
+        // Set a variable to the Documents path.
+        string docPath =
+          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        // Write the string array to a new file named "Challenges.txt".
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Challenges.txt")))
+        {
+            foreach (string line in lines)
+                outputFile.WriteLine(line);
+        }
+    }
+}
