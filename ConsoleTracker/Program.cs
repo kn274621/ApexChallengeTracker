b5eff3e98021c87bc7db4938568faddd8596646c
Karli Nadsady <122899636+kn274621@users.noreply.github.com>
2024-09-18 10:24:19 +0000

    added menu function

--- ConsoleTracker/Program.cs
+++ ConsoleTracker/Program.cs
@@ -1,17 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
-        if 
-
-        // Create a string array with the lines of text
-        string[] lines = ["First line", "Second line", "Third line"];
-
-        // Set a variable to the Documents path.
+        // Set a variable to the Documents path to read previous challenges data
         string docPath =
           Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        // Create string lists with the challenge options & the printed data
+        List<string> challengeOptions = new List<string>();
+        List<string> lines = new List<string>();
+
+        bool isDone = false;
+
+        while (isDone == false)
+        {
+            int four = Program.Menu();
+        }
+
+
+
+
 
         // Write the string array to a new file named "Challenges.csv".
         using (StreamWriter outStream = new(Path.Combine(docPath, "Challenges.csv")))
@@ -20,5 +33,24 @@ class Program
             foreach (string line in lines)
                 outStream.WriteLine(line);
         }
+    }
+    static int Menu()
+    {
+        Console.Write("Welcome to the Apex Legends Challenge Tracker\n" +
+                      "Options:\n" +
+                      "1. Add challenges\n" +
+                      "2. Remove challenges\n" +
+                      "3. View challenges\n" +
+                      "4. Exit & Export\n"+
+                      "\nEnter your choice (1, 2, 3, 4): ");
+
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 4.");
+            Console.Write("\nEnter your choice (1, 2, 3, 4): ");
+        }
+
+        return choice;
     }
 }
